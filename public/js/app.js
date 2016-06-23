c94f41f5ada712b684f6fa309fc112d2b26c8292
UberBooster <info@uberbooster.com>
2016-06-23 19:16:04 -0400

    set button functionality for all buttons

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,9 @@
+var func;
+$.fn.hasAttr = function(name) {
+   return (typeof this.attr(name) !== 'undefined' && this.attr(name) !== false);
+};
+
+
 $(document).ready(function(){
   var pauseBtn = $('#pause');
   var startBtn = $('#start-work');
@@ -10,28 +16,31 @@ $(document).ready(function(){
   var working = 0;
   var onShortBreak = 0;
   var onLongBreak = 0;
+  var reset = 0;
+  var pomodoros = 0;
+  var workSessions = 0;
+  var shortBreakSessions = 0;
+  var longBreakSessions = 0;
 
 
   startBtn.on('click', startButtonClicked);
-  pauseBtn.on('click', pauseTimer);
+  pauseBtn.on('click', pauseButtonClicked);
   shortBreakBtn.on('click', shortBreakButtonClicked);
-
+  longBreakBtn.on('click', longBreakButtonClicked);
+  resetBtn.on('click', resetButtonClicked);
 
   function startButtonClicked(){
     working = 1;
+    onShortBreak = 0;
+    onLongBreak = 0;
+    workSessions = workSessions + 1;
+    minutes.text('00');
+    seconds.text('04')
+    checkButtonStatus();
     startCountdown();
   };
 
-  function shortBreakButtonClicked(){
-    onShortBreak = 1; // flags that the short break button was clicked
-    minutes.text('05'); // sets minutes to 5
-    seconds.text('00'); // sets seconds to 00
-    shortBreakBtn.addClass("disabled");
-    shortBreakBtn.attr("disabled",'disabled');
-    startCountdown(); // starts the countdown
-  };
-
-  function pauseTimer(){ // this function only runs when the pause button is clicked (or the resume button)
+  function pauseButtonClicked(){ // this function only runs when the pause button is clicked (or the resume button)
     if (pause === 0){ // if pause = 0 it means the pause button was clicked.  If pause = 1 it means the resume button was clicked
       pause = 1; // sets the pause variable flag to 1, which will clear the interval (stop the clock) in the startCountdown function
       pauseBtn.text('Resume'); // changes the text on the pause button to resume
@@ -42,25 +51,130 @@ $(document).ready(function(){
     };
   };
 
-  function startCountdown(){
-    if (working === 1){ // if the start work button was clicked
-      startBtn.addClass("disabled");
-      startBtn.attr("disabled",'disabled');
-      pauseBtn.removeClass('disabled');
-      pauseBtn.removeAttr('disabled');
-    };
+  function shortBreakButtonClicked(){
+    onShortBreak = 1; // flags that the short break button was clicked
+    shortBreakSessions = shortBreakSessions + 1;
+    minutes.text('00'); // sets minutes to 5
+    seconds.text('05'); // sets seconds to 00
+    startCountdown(); // starts the countdown
+  };
 
+  function longBreakButtonClicked(){
+    onLongBreak = 1;
+    longBreakSessions = longBreakSessions + 1;
+    minutes.text('00'); // sets minutes to 5
+    seconds.text('06'); // sets seconds to 00
+    longBreakBtn.addClass("disabled");
+    longBreakBtn.attr("disabled",'disabled');
+    startCountdown(); // starts the countdown
+  };
+
+  function resetButtonClicked(){
+    reset = 1;
+    pause = 0;
+    working = 0;
+    onShortBreak = 0;
+    onLongBreak = 0;
+    pomodoros = 0;
+    workSessions = 0;
+    shortBreakSessions = 0;
+    longBreakSessions = 0;
+    minutes.text('00');
+    seconds.text('07');
+    setButtonsToWaitingToWork();
+    document.getElementsByTagName("TITLE")[0].innerHTML = "25:00 Pomodoro Timer"  // this puts the timer in the title tab
+  };
+
+
+
+
+
+  function checkButtonStatus(){
+    if (reset === 1){
+        reset = 0;
+        setButtonsToWaitingToWork();
+    } else {
+      if (working === 0 && onShortBreak === 0 && workingSessions === 0 && shortBreakSessions === 0 && onLongBreak === 0 && pomodoros === 0){ //this means the person is currently working
+        // set buttons for working status
+        //
+      } else {
+        if (working === 1 && onShortBreak === 1 && pomodoros < 3){
+          // set buttons for a short break
+        } else {
+          if(working === 1 && onShortBreak === 0 && pomodoros ===3){
+            // set buttons for working status, but prepare longBreakBtn to be enabled
+          } else {
+
+          }
+
+        }
+      }
+    }
+  };
+
+
+
+  function setButtonsToWaitingToWork(){
+    if (!pauseBtn.hasClass("disabled")){pauseBtn.addClass("disabled")};
+    if (!pauseBtn.hasAttr("disabled")){pauseBtn.attr("disabled",'disabled')};
+    if (startBtn.hasClass("disabled")){startBtn.removeClass('disabled')};
+    if (startBtn.hasAttr("disabled")){startBtn.removeAttr('disabled','disabled')};
+    if (!shortBreakBtn.hasClass("disabled")){shortBreakBtn.addClass("disabled")};
+    if (!shortBreakBtn.hasAttr("disabled")){shortBreakBtn.attr("disabled",'disabled')};
+    if (!longBreakBtn.hasClass("disabled")){longBreakBtn.addClass("disabled")};
+    if (!longBreakBtn.hasAttr("disabled")){longBreakBtn.attr("disabled",'disabled')};
+  };
+
+  function setButtonsToTimerRunning(){
+    if (pauseBtn.hasClass("disabled")){pauseBtn.removeClass("disabled")};
+    if (pauseBtn.hasAttr("disabled")){pauseBtn.removeAttr('disabled','disabled')};
+    if (!startBtn.hasClass("disabled")){startBtn.addClass('disabled')};
+    if (!startBtn.hasAttr("disabled")){startBtn.attr('disabled','disabled')};
+    if (!shortBreakBtn.hasClass("disabled")){shortBreakBtn.addClass("disabled")};
+    if (!shortBreakBtn.hasAttr("disabled")){shortBreakBtn.attr("disabled",'disabled')};
+    if (!longBreakBtn.hasClass("disabled")){longBreakBtn.addClass("disabled")};
+    if (!longBreakBtn.hasAttr("disabled")){longBreakBtn.attr("disabled",'disabled')};
+  };
+  func = setButtonsToWaitingForLongBreak;
+
+  function setButtonsToWaitingForShortBreak(){
+    if (!pauseBtn.hasClass("disabled")){pauseBtn.addClass("disabled")};
+    if (!pauseBtn.hasAttr("disabled")){pauseBtn.attr('disabled','disabled')};
+    if (!startBtn.hasClass("disabled")){startBtn.addClass('disabled')};
+    if (!startBtn.hasAttr("disabled")){startBtn.attr('disabled','disabled')};
+    if (shortBreakBtn.hasClass("disabled")){shortBreakBtn.removeClass("disabled")};
+    if (shortBreakBtn.hasAttr("disabled")){shortBreakBtn.removeAttr("disabled",'disabled')};
+    if (!longBreakBtn.hasClass("disabled")){longBreakBtn.addClass("disabled")};
+    if (!longBreakBtn.hasAttr("disabled")){longBreakBtn.attr("disabled",'disabled')};
+  };
+
+  function setButtonsToWaitingForLongBreak(){
+    if (!pauseBtn.hasClass("disabled")){pauseBtn.addClass("disabled")};
+    if (!pauseBtn.hasAttr("disabled")){pauseBtn.attr('disabled','disabled')};
+    if (!startBtn.hasClass("disabled")){startBtn.addClass('disabled')};
+    if (!startBtn.hasAttr("disabled")){startBtn.attr('disabled','disabled')};
+    if (!shortBreakBtn.hasClass("disabled")){shortBreakBtn.addClass("disabled")};
+    if (!shortBreakBtn.hasAttr("disabled")){shortBreakBtn.attr("disabled",'disabled')};
+    if (longBreakBtn.hasClass("disabled")){longBreakBtn.removeClass("disabled")};
+    if (longBreakBtn.hasAttr("disabled")){longBreakBtn.removeAttr("disabled",'disabled')};
+  };
+
+
+
+
+  function startCountdown(){
+    pauseBtn.removeClass('disabled'); // enables the pause button
+    pauseBtn.removeAttr('disabled');
+    clearInerval(coundown);
     var countdown = setInterval(function(){
       var secondsVal = +seconds.text(); // the + sign makes this behave like a number
       var minutesVal = +minutes.text();
       if(pause === 1){ // checks to see if the pause button was clicked
         clearInterval(countdown);
       };
-      if (secondsVal === 0 && minutesVal ===0){ // the tinmer has run down to 00:00
-        shortBreakBtn.removeClass('disabled');
-        shortBreakBtn.removeAttr('disabled');
+      if ((secondsVal === 0 && minutesVal ===0) || reset === 1){ // the timer has run down to 00:00 or the reset button was clicked
         clearInterval(countdown);
-        working = 0;
+        checkButtonStatus();
         return;
       };
       if(secondsVal === 0){
@@ -78,7 +192,7 @@ $(document).ready(function(){
         }
       };
       // console.log(typeof +secondsVal);
-      document.getElementsByTagName("TITLE")[0].innerHTML = minutes.text()+":"+seconds.text()+" PTimer"
+      document.getElementsByTagName("TITLE")[0].innerHTML = minutes.text()+":"+seconds.text()+" Pomodoro Timer"  // this puts the timer in the title tab
     }, 1000);
   };
 });
